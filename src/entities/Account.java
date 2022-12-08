5c6cc76fb3b7f377e679eaa39abdd76c013f33d0
flino1934 <f.lino1934@hotmail.com>
2022-12-08 19:07:57 -0300

    Realizando o primeiro teste unitario de deposito quando houver saldo positivo
    Signed-off-by: flino1934 <f.lino1934@hotmail.com>

---
+++ src/entities/Account.java
@@ -0,0 +1,59 @@
+package entities;
+
+public class Account {
+
+	public static double DEPOSIT_FEE_PERCENTAGE = 0.02;
+
+	private Long id;
+	private Double balance;
+
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Account(Long id, Double balance) {
+		this.id = id;
+		this.balance = balance;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void deposit(double amount) {
+
+		if (amount > 0) {
+			amount -= amount * DEPOSIT_FEE_PERCENTAGE;
+			this.balance += amount;
+		}
+	}
+
+	public void withdraw(double amount) {
+
+		if (amount > balance) {
+
+			throw new IllegalArgumentException("Saldo Insuficiente");
+
+		}
+
+		this.balance -= amount;
+
+	}
+
+	public double fullWithdraw() {
+		
+		double aux = balance;
+		balance = 0.0;
+
+		return aux;
+	}
+
+}
